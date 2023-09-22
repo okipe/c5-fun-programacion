417319b9f6cecfec45ce809666317fc730cf6770
Oscar Roman <oscarroman.pe@gmail.com>
2023-09-21 21:15:50 -0500

    Clase 6

---
+++ semana-6/ordenamiento_datos1/ordenamiento_datos1/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace ordenamiento_datos1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtNumeros = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.lblOrdenar = new System.Windows.Forms.Button();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.btnQuicksort = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtNumeros
+            // 
+            this.txtNumeros.Location = new System.Drawing.Point(113, 127);
+            this.txtNumeros.Name = "txtNumeros";
+            this.txtNumeros.Size = new System.Drawing.Size(304, 20);
+            this.txtNumeros.TabIndex = 0;
+            this.txtNumeros.Text = "1,2,3,4";
+            this.txtNumeros.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening);
+            // 
+            // lblOrdenar
+            // 
+            this.lblOrdenar.Location = new System.Drawing.Point(80, 181);
+            this.lblOrdenar.Name = "lblOrdenar";
+            this.lblOrdenar.Size = new System.Drawing.Size(108, 23);
+            this.lblOrdenar.TabIndex = 2;
+            this.lblOrdenar.Text = "Ordenar Burbujha";
+            this.lblOrdenar.UseVisualStyleBackColor = true;
+            this.lblOrdenar.Click += new System.EventHandler(this.btnOrdenar_Click);
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Location = new System.Drawing.Point(113, 225);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(304, 20);
+            this.txtResultado.TabIndex = 3;
+            this.txtResultado.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            // 
+            // btnQuicksort
+            // 
+            this.btnQuicksort.Location = new System.Drawing.Point(220, 180);
+            this.btnQuicksort.Name = "btnQuicksort";
+            this.btnQuicksort.Size = new System.Drawing.Size(117, 23);
+            this.btnQuicksort.TabIndex = 4;
+            this.btnQuicksort.Text = "Ordenar Quicksort";
+            this.btnQuicksort.UseVisualStyleBackColor = true;
+            this.btnQuicksort.Click += new System.EventHandler(this.btnQuicksort_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnQuicksort);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.lblOrdenar);
+            this.Controls.Add(this.txtNumeros);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNumeros;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Button lblOrdenar;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.Button btnQuicksort;
+    }
+}
+
---
+++ semana-6/ordenamiento_datos1/ordenamiento_datos1/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ordenamiento_datos1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnOrdenar_Click(object sender, EventArgs e)
+        {
+            // llenar los valores de un textbox a un arreglo
+            string textoNumeros = txtNumeros.Text;
+            string[] valores = textoNumeros.Split(',');
+            int[] m = new int[valores.Length];
+
+            // cargar valores de tipo text a una matriz de tipo integer
+            for (int i = 0; i < valores.Length; i++)
+            {
+                m[i] = int.Parse(valores[i]);
+            }
+
+            int auxiliar;
+            for (int i = 0; i < m.Length; i++)
+            {
+                for (int j = 0; j < m.Length -1; j++)
+                {
+                    if (m[j] > m[j + 1])
+                    {
+                        auxiliar = m[j];
+                        m[j] = m[j + 1];
+                        m[j + 1] = auxiliar;    
+                    }
+                }
+            }
+
+            string texto = "";
+            for (int i = 0; i < m.Length; i++)
+            {
+                texto += m[i].ToString() + ", ";
+            }
+
+            texto = texto.Substring(0, texto.Length - 2);
+            // Reportando el resultado
+            txtResultado.Text = texto;
+            txtResultado.ForeColor = Color.Red;
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnQuicksort_Click(object sender, EventArgs e)
+        {
+            // llenar los valores de un textbox a un arreglo
+            string textoNumeros = txtNumeros.Text;
+            string[] valores = textoNumeros.Split(',');
+            int[] m = new int[valores.Length];
+
+            // cargar valores de tipo text a una matriz de tipo integer
+            for (int i = 0; i < valores.Length; i++)
+            {
+                m[i] = int.Parse(valores[i]);
+            }
+
+            Quicksort(m,0, m.Length - 1); 
+
+        }
+        public static void Quicksort(int[] arreglo, int izquierda, int derecha)
+        {
+            if (izquierda < derecha)
+            {
+                int indicePivote = Particionar(arreglo, izquierda, derecha);
+
+                Quicksort(arreglo, izquierda, indicePivote - 1);
+                Quicksort(arreglo, indicePivote + 1, derecha);
+            }
+        }
+
+        private static int Particionar(int[] arreglo, int izquierda, int derecha)
+        {
+            int pivote = arreglo[derecha];
+            int i = izquierda - 1;
+            for (int j = izquierda; j < derecha; j++)
+            {
+                if (arreglo[j] <= pivote)
+                {
+                    i++;
+                    int temp = arreglo[i];
+                    arreglo[i] = arreglo[j];
+                    arreglo[j] = temp;
+                    int temp2 = arreglo[i + 1];
+                    arreglo[i + 1] = arreglo[derecha];
+                }
+            }
+        }
+    }
+}
